e7084b5a0346bfae1bced4a9d02df67e322d5f18
Jul Lang <jullanggit@proton.me>
2026-06-01 21:56:39 +0200

    package immich-uplaod-optimizer

---
+++ pkgs/by-name/im/immich-upload-optimizer/package.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  pkgs,
+  buildGoModule,
+  fetchFromGitHub,
+  makeWrapper,
+}:
+
+buildGoModule rec {
+  pname = "immich-upload-optimizer";
+  version = "0.5.5";
+
+  src = fetchFromGitHub {
+    owner = "miguelangel-nubla";
+    repo = "immich-upload-optimizer";
+    tag = "v${version}";
+    hash = lib.fakeHash;
+  };
+
+  vendorHash = lib.fakeHash;
+
+  meta = {
+    description = "Automatically optimize files uploaded to Immich in order to save storage space";
+    homepage = "https://github.com/miguelangel-nubla/immich-upload-optimizer";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ jullang ]; # TODO: Add yourself in maintainers/maintainer-list.nix
+    mainProgram = "immich-upload-optimizer";
+  };
+}
